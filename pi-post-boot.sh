1298057fea5ff200bb9ab914ca3bb0234d00becb
ozonator <70418288+ozonator@users.noreply.github.com>
2022-01-25 22:57:22 -0500

    Update and rename post-boot.sh to pi-post-boot.sh

--- pi-post-boot.sh
+++ pi-post-boot.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-# turn off the HDMI port
-# note: needs revision for Bullseye
+
+# things I run on a Raspberry Pi after boot
+
+# I don't put this in anything that runs automatically at boot time, since I want to see that the Pi is running normally first.
+# The LEDs are handy for that, and since I tend to run Pis headless, it's useful to leave the HDMI port on, in case I need to
+# plug in a display to see what's going on if a Pi is unreachable after boot.
+#
+# Nothing original here - just a couple of things to run manually to save power after boot time.
+
+# turn off the HDMI port to save power
+# note: may need revision for Bullseye
 /opt/vc/bin/tvservice --off
 
 ### configure LEDs for B-sized Pis like the 2B, 3A+, 4B
@@ -17,7 +26,7 @@ echo 0 | sudo tee /sys/class/leds/led1/brightness
 # Optional: set the activity LED to trigger on cpu0 instead of mmc0 (SD card access)
 #echo cpu0 | sudo tee /sys/class/leds/led0/trigger
 
-### comment the above and uncomment below to configure LEDs for a Pi Zero
+### comment the above and uncomment the 'echo' lines below to configure LEDs for a Pi Zero
 
 # Set the Pi Zero activity LED trigger to 'none'
 #echo none | sudo tee /sys/class/leds/led0/trigger
